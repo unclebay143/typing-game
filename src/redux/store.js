c57cac0b8a2d741579337114b80143d50250cb3a
unclebay <unclebigbay@gmail.com>
2021-06-07 12:55:24 +0100

    SETUP REDUX-THUNK SETUP CONDITION FOR NODE_ENV FOR MIDDLEWARE (DEV TOOL)

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,23 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './rootReducer';
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./rootReducer";
 
 const middleware = [thunk];
 
+//  Middleware to use when in development mode (redux dev tool)
+const devCompose = compose(
+  applyMiddleware(...middleware),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
-const store = createStore(
-        rootReducer,
-        compose(applyMiddleware(...middleware))
-        // compose(applyMiddleware(...middleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-    )
+// Middleware to apply use when in production mode (redux dev tool causing issues)
+const prodCompose = compose(applyMiddleware(...middleware));
 
+// Check the node environment status and apply compose accordingly
+const useCompose =
+  process.env.NODE_ENV === "production" ? prodCompose : devCompose;
+
+// Store instance
+const store = createStore(rootReducer, useCompose);
 
 export default store;
-
--- src/redux/user/reducers/user.reducers.js
+++ src/redux/user/reducers/user.reducers.js
@@ -1,40 +1,46 @@
-import { REGISTRATION_SUCCESSFUL, LOGIN_SUCCESSFULL, SET_ERROR } from "./../../types/index";
+import {
+  SET_ERROR,
+  REGISTRATION_SUCCESSFUL,
+  LOGIN_SUCCESSFULL,
+  USER_LOADED,
+} from "./../../types/index";
 
 const INITIAL_STATE = {
-    user: null,
-    error: null,
-    isLoggedIn: false
-}
+  user: null,
+  error: null,
+  isLoggedIn: false,
+};
 
 // redux reducer function
-const userReducer = (state=INITIAL_STATE, action) =>{
-    // Destructure type and payload from action
-    const { type, payload } = action;
+const userReducer = (state = INITIAL_STATE, action) => {
+  // Destructure type and payload from action
+  const { type, payload } = action;
 
-    switch (type) {
-        case SET_ERROR: // Handles all errors, i.e no need for registration/login error cases
-            return({
-                ...state,
-                error: payload
-            })
-        case REGISTRATION_SUCCESSFUL:
-            return({
-                    ...state, 
-                })
-        case LOGIN_SUCCESSFULL:
-            return({
-                    ...state,
-                    isLoggedIn: true
-            })
-        case USER_LOADED:
-            return({
-                ...state,
-                user: payload
-            })
-    
-        default: // return the state by default else react will shout
-            return state;
-    }
-}
+  switch (type) {
+    case SET_ERROR: // Handles all errors, i.e no need for registration/login error cases
+      return {
+        ...state,
+        error: payload,
+      };
+    case REGISTRATION_SUCCESSFUL:
+      return {
+        ...state,
+      };
+    case LOGIN_SUCCESSFULL:
+      return {
+        ...state,
+        isLoggedIn: true,
+      };
+    case USER_LOADED:
+      return {
+        ...state,
+        user: payload,
+      };
 
-export default userReducer
+    default:
+      // return the state by default else react will shout
+      return state;
+  }
+};
+
+export default userReducer;
